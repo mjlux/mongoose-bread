2f260df57ac73e49831c88c92b33b78ba9bf551e
mjlux <michaeljohanneslux@gmail.com>
2024-02-05 21:22:38 +0100

    implement optional AtlasSearch in browse method

--- src/factories/browseFactory.js
+++ src/factories/browseFactory.js
@@ -13,12 +13,49 @@ function browseFactory(pluginOptions) {
     [readCount]: 0,
   });
 
+  function runAtlasSearch(model, options) {
+    const { query, paginateOptions } = options;
+    const { limit, projection } = paginateOptions;
+
+    const pipeline = [query, { $limit: limit }];
+
+    if (projection) {
+      pipeline.push({ $project: { ...projection } });
+    }
+
+    return model.aggregate(pipeline).then((docs) => {
+      return {
+        docs,
+        pagination: {
+          totalDocs: docs.length,
+          offset: 0,
+          limit: limit,
+          totalPages: 1,
+          page: 1,
+          pagingCounter: 1,
+          hasPrevPage: false,
+          hasNextPage: false,
+          prevPage: null,
+          nextPage: null,
+        },
+      };
+    });
+  }
+
   return function browse(options) {
     options = { ...pluginOptions, ...options };
 
-    const { query, paginateOptions } = options;
+    const { query, paginateOptions, enableAtlasSearch, atlasSearchIndex } =
+      options;
     const { lean, leanWithId, leanWithout_id, customLabels, customCount } =
       paginateOptions;
+
+    if (enableAtlasSearch && atlasSearchIndex) {
+      return runAtlasSearch(this, options)
+        .then(remove_id)
+        .then(toBreadResult)
+        .catch(toBreadError);
+    }
 
     if (
       customCount &&
