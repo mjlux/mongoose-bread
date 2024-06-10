1fe8cce23f8a44f1b64660f42fe4c56c40b097dc
mjlux <michaeljohanneslux@gmail.com>
2024-06-10 23:04:20 +0200

    convert rehabilitateFactory from js to ts

---
+++ src/factories/rehabilitateFactory.ts
@@ -0,0 +1,31 @@
+import { SoftDeleteModel } from "mongoose-delete";
+import { MongooseBreadOptions, MongooseBreadRehabilitateOptions } from "../../types";
+import { Model } from "mongoose";
+
+const toBreadErrorFactory = require("./toBreadErrorFactory");
+
+/**
+ * Factory function to create Model.rehabilitate() method
+ * @param {import('../index').MongooseBreadOptions} pluginOptions Config of mongoose-bread plugin
+ */
+function rehabilitateFactory(pluginOptions: MongooseBreadOptions) {
+	const { docs, modifiedCount, acknowledged } = pluginOptions.customLabels;
+	const toBreadResult = ([result, _docs]: [any, any[]]) => ({
+		[docs]: _docs,
+		[acknowledged]: result.acknowledged ?? true,
+		[modifiedCount]: result.modifiedCount ?? result.nModified,
+	});
+	const toBreadError = toBreadErrorFactory({
+		[docs]: [],
+		[acknowledged]: false,
+		[modifiedCount]: 0,
+	});
+
+	return function rehabilitate(this: Model<any> & SoftDeleteModel<any>, options: MongooseBreadRehabilitateOptions) {
+		const { query } = options;
+		const mergeRestoreAndDocs = (_docs: any) => Promise.all([this.restore(query), Promise.resolve(_docs)]);
+		return this.findWithDeleted(query, { _id: 1 }).then(mergeRestoreAndDocs).then(toBreadResult).catch(toBreadError);
+	};
+}
+
+module.exports = rehabilitateFactory;
